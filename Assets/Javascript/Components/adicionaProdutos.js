a84b5ef885885815256934b74c7abb1e6bc89cde
Gisa99 <rgisele644@gmail.com>
2024-06-11 08:55:17 -0300

    validação formulário add produtos

--- Assets/Javascript/Components/adicionaProdutos.js
+++ Assets/Javascript/Components/adicionaProdutos.js
@@ -2,8 +2,7 @@ import { conectaApi } from "../API/conectaAPI.js";
 
 const formulario = document.querySelector("[data-form]");
 
-
-async function adicionarProdutos(evento){
+async function adicionarProdutos(evento) {
     evento.preventDefault();
 
     const imageURL = document.querySelector("[data-imagem]").value;
@@ -12,15 +11,19 @@ async function adicionarProdutos(evento){
     const price = document.querySelector("[data-preco]").value;
     const description = document.querySelector("[data-descricao]").value;
 
-    
+    const camposForm = [imageURL, section, name, price, description];
 
-   await conectaApi.adicionaProdutos(imageURL, section, name, price, description)
+    // Verificação de campos vazios
+    let camposVazios = camposForm.some(campo => campo.trim() === "");
 
-   alert('produto adicionado com sucesso!')
+    if (camposVazios) {
+        alert('Nenhum campo deve estar vazio');
+    } else {
+        await conectaApi.adicionaProdutos(imageURL, section, name, price, description);
 
-   window.location = "./renderizaTodos.html"
-
+        alert('Produto adicionado com sucesso!');
+        window.location = "./renderizaTodos.html";
+    }
 }
 
-formulario.addEventListener("submit", evento => adicionarProdutos(evento));
-
+formulario.addEventListener("submit", adicionarProdutos);
